0d43614ba8233fb1d987b0fdad321ecc3f34a7a9
Takahisa <samuel.y.yamaguchi@mail.com>
2017-10-02 00:03:45 -0300

    express index

--- clarion.js
+++ clarion.js
@@ -1,5 +1,16 @@
 const Eris = require("eris");
 
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+
 var bot = new Eris("MzYzODQ1NTI2NzExNTY2MzM2.DLMrYg.zG28pp2E8PR43uY3subsXrzFblI");
 bot.on("ready", () => {
     console.log("Ready!");
