525f9f24575fd353b814700a6d61e3f3a0cfa52f
Takahisa <samuel.y.yamaguchi@mail.com>
2017-10-02 00:11:20 -0300

    express index 4

--- clarion.js
+++ clarion.js
@@ -2,9 +2,13 @@ const Eris = require("eris");
 
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.get('*', function(req, res){
   res.sendfile(__dirname + '/public/index.html');
+});
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
 
 
