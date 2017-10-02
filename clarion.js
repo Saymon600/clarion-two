03774d199dd03ec86ff665a251a846f9c6bc39ee
Takahisa <samuel.y.yamaguchi@mail.com>
2017-10-02 00:07:09 -0300

    express index 3

--- clarion.js
+++ clarion.js
@@ -3,7 +3,6 @@ const Eris = require("eris");
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
 app.get('*', function(req, res){
   res.sendfile(__dirname + '/public/index.html');
 });
