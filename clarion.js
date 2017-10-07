444b374a42f71092ae0296e010f92ac3c98b42be
Takahisa <samuel.y.yamaguchi@mail.com>
2017-10-07 07:09:40 -0300

    bastao teste 20

--- clarion.js
+++ clarion.js
@@ -168,17 +168,18 @@ bot.on("messageCreate", (msg) => {
         members.forEach(function(member){
         	if(member.id == msg.author.id){
         		var roles = member.roles;
+        		console.log(roles);
 	        	var hadRole = false;
 	        	roles.forEach(function(role){
-	        		if(role.id == firekeeper){
+	        		if(role == firekeeper){
 	        			bot.removeGuildMemberRole(msg.channel.guild.id,msg.author.id,firekeeper,"Finished story on bonfire");
-	                	bot.createMessage(msg.channel.id, "本当にもう終わりなの？");
+	                	//bot.createMessage(msg.channel.id, "本当にもう終わりなの？");
 	                	hadRole = true;
 	        		}
 	        	});
 	        	if(!hadRole){
 	                bot.addGuildMemberRole(msg.channel.guild.id,msg.author.id,firekeeper,"Starting story on bonfire");
-	                bot.createMessage(msg.channel.id, 'You are the firekeeper now, make sure to tell us a good story.');
+	                //bot.createMessage(msg.channel.id, 'You are the firekeeper now, make sure to tell us a good story.');
                	}
         	}
        	});
