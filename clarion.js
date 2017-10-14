69b995a41bb7e9c40e1d24960741b0b6654c89e0
Augus-top <acpluschkat@hotmail.com>
2017-10-13 21:36:28 -0300

    Creating new connections at every request

--- clarion.js
+++ clarion.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 const fs = require('fs');
 const request = require('request');
 const { Client } = require('pg')
-const client = new Client({
+var client = new Client({
   user: 'izhdpobumyufya',
   host: 'ec2-54-221-207-192.compute-1.amazonaws.com',
   database: 'd5qsftiankav99',
@@ -458,6 +458,13 @@ bot.on("messageCreate", (msg) => {
 });
 
 function testDB(msg, type){
+    client = new Client({
+          user: 'izhdpobumyufya',
+          host: 'ec2-54-221-207-192.compute-1.amazonaws.com',
+          database: 'd5qsftiankav99',
+          password: '32bc5c61e1d03e330f66dbd402ba9628e559e924d82477a847ad2cd99b39174e',
+          port: 5432,
+        });
     client.connect();
     sql = "select * from perverts where id = $1 and hentai_type = $2;"
     sqlValues = [msg.author.id, type]
