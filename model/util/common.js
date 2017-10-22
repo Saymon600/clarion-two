d3f87c751304d7607f9d940cc7807fe4656aadcf
Augus-top <acpluschkat@hotmail.com>
2017-10-22 09:14:12 -0200

    Better common functions

--- model/util/common.js
+++ model/util/common.js
@@ -1,34 +1,28 @@
-var members;
-
 module.exports = {
 
 	findMember: function(msg, id){
-		members = msg.channel.guild.members;
-        return members.find(function findName(member){
+        return msg.channel.guild.members.find((member) =>{
     	    return member.id === id;
         });
 	},
 
 	findMemberByName: function(msg, name){
-		members = msg.channel.guild.members;
-        return members.find(function findName(member){
+        return msg.channel.guild.members.find((member) =>{
     	    return member.username === name || member.nick === name;
         });
 	},
 
 	findIfHasRole: function(msg, memberId, role){
-		const member = this.findMember(msg, memberId);
-		return member.roles.find(function findRole(memberRole){
+		return this.findMember(msg, memberId).roles.find((memberRole) =>{
     	    return memberRole === role;
         });
 	},
 
 	findMemberWithRole: function(msg, role){
-		members = msg.channel.guild.members;
-		return members.find(function findOverallRole(member){
-			return member.roles.find(function findRole(memberRole){
-				return memberRole === role;
-			});
-		});
+		return msg.channel.guild.members.find((member) =>{
+			return member.roles.find((memberRole) =>{
+				return memberRole === role
+			})
+		})
 	}
 }
