fb681aeda1f99292dcb8fb5ec6381242ed4eddbb
Takahisa <samuel.y.yamaguchi@mail.com>
2017-12-02 15:42:10 -0200

    Suco #4

--- model/sucoChannel.js
+++ model/sucoChannel.js
@@ -6,7 +6,7 @@ module.exports = {
 	suco: function(msg, bot){
         if(common.findIfHasRole(msg, msg.author.id, constants.SUCO_ROLE)){
             bot.removeGuildMemberRole(msg.channel.guild.id,msg.author.id,constants.SUCO_ROLE);
-            bot.createMessage(msg.channel.id, "Suco role removed");
+            bot.createMessage(msg.channel.id, "~~Sal~~ Suco role removed");
         }else{
             bot.addGuildMemberRole(msg.channel.guild.id,msg.author.id, constants.SUCO_ROLE);
             bot.createMessage(msg.channel.id, 'Suco role added');
